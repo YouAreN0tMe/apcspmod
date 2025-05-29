0a7992e6fc1885de28b48d835c8825ff06bac2c1
YouAreN0tMe <micampos2028@falmouthschools.org>
2025-05-29 15:36:42 -0400

    first commit

--- src/main/java/apcsp/modid/Apcspmod.java
+++ src/main/java/apcsp/modid/Apcspmod.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import apcsp.modid.item.ModItems;
+
 public class Apcspmod implements ModInitializer {
 	public static final String MOD_ID = "apcspmod";
 
@@ -18,7 +20,7 @@ public class Apcspmod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/apcsp/modid/item/ModItems.java
@@ -0,0 +1,17 @@
+package apcsp.modid.item;
+
+import apcsp.modid.Apcspmod;
+import net.minecraft.item.Item;
+import net.minecraft.registry;
+
+public class ModItems {
+
+    private static Item registerItem(String name, Item item) {
+        return Registry;
+    }
+
+    public static void registerModItems() {
+        Apcspmod.LOGGER.info("Registering Mod Items for " + Apcspmod.MOD_ID);
+
+    }
+}
