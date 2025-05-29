7c4001d40ab219749bca75edee8a75ea6d090c42
YouAreN0tMe <micampos2028@falmouthschools.org>
2025-05-29 15:08:43 -0400

    Default Template

---
+++ src/client/java/apcsp/modid/ApcspmodClient.java
@@ -0,0 +1,10 @@
+package apcsp.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ApcspmodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
